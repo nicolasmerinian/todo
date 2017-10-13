98b17a7ba867a7eb9e8da52352d89b8d71a4d815
Nicolas MERINIAN <nicolasmerinian>
2017-10-13 16:13:57 +0200

    affichage de todos statiques

--- vanilla/src/js/main.js
+++ vanilla/src/js/main.js
@@ -1,3 +1,18 @@
 (function() {
   console.log('ready');
+
+  const TODOS = [
+    { text: 'My first todo' }
+  ];
+
+  displayList(TODOS, document.querySelector('.list'));
 })();
+
+let displayList = (todos, containerElement) => {
+  let listElement;
+  todos.forEach((todo) => {
+    itemElement = document.createElement('li');
+    itemElement.textContent = todo.text;
+    containerElement.appendChild(itemElement);
+  });
+};
