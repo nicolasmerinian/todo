24d0cf820e8e5743b3f15b2ae01b7ef0afbbd9d4
Nicolas MERINIAN <nicolasmerinian>
2017-10-14 00:16:44 +0200

    adding delete feature

--- vanilla/src/js/main.js
+++ vanilla/src/js/main.js
@@ -20,8 +20,9 @@ let makeList = (todos, listElement) => {
     contentElement = document.createElement('span');
     deleteElement = document.createElement('span');
 
+    itemElement.className = 'todo__item';
     contentElement.textContent = todo.text;
-    deleteElement.className += '  delete';
+    deleteElement.className = 'todo__item-delete';
     deleteElement.textContent = 'X';
 
     itemElement.appendChild(contentElement);
@@ -36,8 +37,8 @@ let emptyElement = (element) => {
   }
 };
 
-let addButtonHandler = (e, todos, listElement) => {
-  e.stopPropagation();
+let addButtonHandler = (event, todos, listElement) => {
+  event.stopPropagation();
 
   let inputElement = document.querySelector('.todo__add-input');
 
@@ -56,20 +57,46 @@ let addTodo = (todos, todoText) => {
   todos.push(newTodo);
 };
 
+let deleteButtonHandler = (event, todos, listElement) => {console.log('del');
+  event.stopPropagation();
+
+  if (!event.target.classList.contains('todo__item-delete')) {
+    return;
+  }
+
+  let todoContent = event.target.parentElement.children[0].textContent;
+
+  deleteTodo(todos, todoContent);
+  displayList(todos, listElement);
+};
+
+let deleteTodo = (todos, todoText) => {
+  for (var i = 0; i < todos.length; i += 1) {
+    if (todos[i].text === todoText) {
+      todos.splice(i, 1);
+      break;
+    }
+  }
+};
+
 (function() {
   console.log('ready');
 
-  const TODOS = [
-    { text: 'My first todo' }
-  ];
+  const TODOS = [];
 
   let listElement = document.querySelector('.todo__list');
+
+  displayList(TODOS, listElement);
+
   let buttonElement = document.querySelector('.todo__add-button');
+  let deleteElement = document.querySelector('.todo__item-delete');
 
   buttonElement.addEventListener('click', (e) => {
     addButtonHandler(e, TODOS, listElement);
   }, false);
 
-  displayList(TODOS, listElement);
+  listElement.addEventListener('click', (e) => {
+    deleteButtonHandler(e, TODOS, listElement);
+  }, false);
 
 })();
