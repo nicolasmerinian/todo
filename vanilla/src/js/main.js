83b562b2515bc2813396b00d8c844416aabc9e99
Nicolas MERINIAN <nicolasmerinian>
2017-10-13 16:35:20 +0200

    add button to create todos

--- vanilla/src/js/main.js
+++ vanilla/src/js/main.js
@@ -1,3 +1,25 @@
+let displayList = (todos, listElement) => {
+  emptyList(listElement);
+  makeList(todos, listElement);
+};
+
+let emptyList = (listElement) => {
+  if (listElement) {
+    while (listElement.firstChild) {
+      listElement.removeChild(listElement.firstChild);
+    }
+  }
+};
+
+let makeList = (todos, listElement) => {
+  let itemElement;
+  todos.forEach((todo) => {
+    itemElement = document.createElement('li');
+    itemElement.textContent = todo.text;
+    listElement.appendChild(itemElement);
+  });
+};
+
 (function() {
   console.log('ready');
 
@@ -6,13 +28,17 @@
   ];
 
   displayList(TODOS, document.querySelector('.list'));
-})();
 
-let displayList = (todos, containerElement) => {
-  let listElement;
-  todos.forEach((todo) => {
-    itemElement = document.createElement('li');
-    itemElement.textContent = todo.text;
-    containerElement.appendChild(itemElement);
-  });
-};
+  document.querySelector('.addTodo button')
+      .addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    let newTodo = {
+      text: document.querySelector('.addTodo input').value
+    };
+    TODOS.push(newTodo);
+
+    e.currentTarget.value = "";
+      displayList(TODOS, document.querySelector('.list'));
+  }, false);
+})();
